fa6fd72ac509e221cbd1d16fda52add55c4b33bf
neko-suki <s1150092@gmail.com>
2019-07-28 18:42:21 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 GOOS=js GOARCH=wasm go build -o test.wasm class.go
+GOOS=js GOARCH=wasm go build -o wrapper.wasm wrapper.go test_struct.go
 curl -sO https://raw.githubusercontent.com/golang/go/go1.12.7/misc/wasm/wasm_exec.js
